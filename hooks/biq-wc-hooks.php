cff96cdd4f971eeee5e04f0991640fd9431c55aa
Bayu Candra <bayucandra@gmail.com>
2016-12-23 13:32:47 +0700

    Finish with 'Related product' section

--- hooks/biq-wc-hooks.php
+++ hooks/biq-wc-hooks.php
@@ -5,6 +5,13 @@
     add_filter( 'woocommerce_enqueue_styles', '__return_false' );
     add_filter( 'loop_shop_columns', function(){ global $biq_sns_settings; return $biq_sns_settings["woocommerce"]["loop_shop_columns"]; } );
     add_filter( 'loop_shop_per_page', function(){  global $biq_sns_settings; return $biq_sns_settings["woocommerce"]["loop_shop_per_page"]; } );
+    add_filter( 'woocommerce_output_related_products_args', function($args = array() ){ 
+        global $biq_sns_settings;
+        $args["post_per_page"] = $biq_sns_settings["woocommerce"]["loop_shop_per_page"];
+        $args["columns"] = $biq_sns_settings["woocommerce"]["loop_shop_columns"];
+//        echo is_array($args);
+        return $args;
+    } );
     function biq_wc_add_to_cart(){
         global $template_uri;
         wp_deregister_script( 'wc-add-to-cart' );
