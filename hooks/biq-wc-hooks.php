b8cf3354b852c94846fd35e3ef7dbed68f62d968
Bayu Candra <bayucandra@gmail.com>
2017-01-03 15:27:41 +0700

    small modif at sidebar, add short description to product-archive when in 'show' mode

--- hooks/biq-wc-hooks.php
+++ hooks/biq-wc-hooks.php
@@ -9,18 +9,25 @@
         global $biq_sns_settings;
         $args["post_per_page"] = $biq_sns_settings["woocommerce"]["loop_shop_per_page"];
         $args["columns"] = $biq_sns_settings["woocommerce"]["loop_shop_columns"];
-//        echo is_array($args);
         return $args;
     } );
+    add_action('wp_enqueue_scripts', 'biq_wc_add_to_cart');
+
+    add_filter( 'get_avatar', 'biq_get_avatar', 1, 5 );
+
+    remove_action( 'woocommerce_review_before', 'woocommerce_review_display_gravatar', 10 );
+    add_action( 'woocommerce_review_before', function( $comment ){
+        echo get_avatar( $comment, apply_filters( 'woocommerce_review_gravatar_size', '70' ), '' );
+    }, 10 );
+    
+    
     function biq_wc_add_to_cart(){
         global $template_uri;
         wp_deregister_script( 'wc-add-to-cart' );
         wp_register_script( 'wc-add-to-cart', $template_uri . '/frontend/js/woocommerce/add-to-cart.min.js', array('jquery'), WC_VERSION, true);
         wp_enqueue_script( 'wc-add-to-cart' );
     }
-    add_action('wp_enqueue_scripts', 'biq_wc_add_to_cart');
-
-    add_filter( 'get_avatar', 'biq_get_avatar', 1, 5 );
+    
     function biq_get_avatar($avatar, $id_or_email, $size, $default, $alt){
         $biq_avatar = 
                 '<div class="biq-avatar">'
@@ -28,8 +35,7 @@
                 . '</div>';
         return $biq_avatar;
     }
-
-remove_action( 'woocommerce_review_before', 'woocommerce_review_display_gravatar', 10 );
-add_action( 'woocommerce_review_before', function( $comment ){
-    echo get_avatar( $comment, apply_filters( 'woocommerce_review_gravatar_size', '70' ), '' );
-}, 10 );
+    function biq_wc_template_loop_product_short_desc(){
+        global $post;
+        echo '<p class="short-desc">'.substr( $post->post_excerpt, 0, 100 ).'</p>';
+    }
