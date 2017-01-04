cdec74f4c768e979ce829dcc46462e6f6c659729
Bayu Candra <bayucandra@gmail.com>
2017-01-04 11:10:49 +0700

    Finish with responsive for all pages for -sm

--- page.php
+++ page.php
@@ -3,20 +3,20 @@
 ?>
     <div class="biq-box-wrapper short biq-container body biq-layout">
         <?php
-            echo do_shortcode( biq_get_shortcode_part( "sidebar", "widget" ) );//LEFT SIDEBAR
-        ?>
-        <?php
             if( have_posts() ):
                 while( have_posts() ):
                     the_post();
         ?>
-            <div class="biq-container biq-box-content left">
+            <div class="biq-container biq-box-content right">
                 <h1 style="text-align:center;"><?php the_title();?></h1>
                 <p><?php the_content();?></p>
             </div>
         <?php
                 endwhile;
             endif;
+        ?>
+        <?php
+            echo do_shortcode( biq_get_shortcode_part( "sidebar", "widget" ) );//LEFT SIDEBAR
         ?>
     </div>
 <?php
--- shortcodes/functions/wrappers.php
+++ shortcodes/functions/wrappers.php
@@ -15,7 +15,8 @@
 	$css_inline = !empty( $css_inline ) ? ' style = "'.$css_inline.'"' : '';
 	$classes = !empty( $classes ) ? ' '.$classes : '';
 	
-	return '<div'.$css_inline.' class="biq-box-wrapper full biq-container'.$classes.'">' . do_shortcode($content) . '</div>';
+	return '<div'.$css_inline.' class="biq-box-wrapper full'.$classes.'">' . do_shortcode($content) . '</div>';
+//	return '<div'.$css_inline.' class="biq-box-wrapper full biq-container'.$classes.'">' . do_shortcode($content) . '</div>';
     }
     add_shortcode('biq_box_wrapper_full', 'biq_box_wrapper_full_shortcode');
     add_shortcode('biq_box_wrapper_full_alt', 'biq_box_wrapper_full_shortcode');
@@ -34,8 +35,27 @@
 	if( !empty( $css_inline ) ) $css_inline = ' style = "'.$css_inline.'"';
 	if( !empty( $classes ) ) $classes = ' '.$classes;
 	
-	return '<div'.$css_inline.' class="biq-box-wrapper short biq-container'.$classes.'">' . do_shortcode($content) . '</div>';
+	return '<div'.$css_inline.' class="biq-box-wrapper short'.$classes.'">' . do_shortcode($content) . '</div>';
+//	return '<div'.$css_inline.' class="biq-box-wrapper short biq-container'.$classes.'">' . do_shortcode($content) . '</div>';
     }
     add_shortcode('biq_box_wrapper_short', 'biq_box_wrapper_short_shortcode');
     add_shortcode('biq_box_wrapper_short_alt', 'biq_box_wrapper_short_shortcode');
+    
+    function biq_box_wrapper_shortcode($atts, $content = null){
+	extract(
+	    shortcode_atts (
+		array(
+		    'classes'=>'',//CSS classes in space separated
+		    'css_inline'=> ''//inline styles
+		),
+		$atts 
+	    ) 
+	);
+	
+	if( !empty( $css_inline ) ) $css_inline = ' style = "'.$css_inline.'"';
+	if( !empty( $classes ) ) $classes = ' '.$classes;
+	
+	return '<div'.$css_inline.' class="biq-box-wrapper'.$classes.'">' . do_shortcode($content) . '</div>';
+    }
+    add_shortcode('biq_box_wrapper', 'biq_box_wrapper_shortcode');
 ?>
