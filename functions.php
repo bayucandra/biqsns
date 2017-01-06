141a1fd2f75041c5616a3e39a4d7d51503e35505
Bayu Candra <bayucandra@gmail.com>
2017-01-06 20:11:17 +0700

    'logo' widget tuneup

--- functions.php
+++ functions.php
@@ -1,4 +1,9 @@
 <?php
+
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
+
 add_theme_support( 'woocommerce' );
 add_theme_support( 'menus' );
 //BEGIN GLOBAL VARS===================
